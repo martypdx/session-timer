f4e151e6cdfa69b210e396ca91241590ba4e4aef
Marty Nelson <marty@martypdx.com>
2024-01-17 19:15:19 -0800

    more refactoring. found bug with latency in display due to needing to round not truncate ms to s

--- main.js
+++ main.js
@@ -7,27 +7,15 @@ if(Notification) {
     }
 }
 
-function notify(message) {
-    if(Notification.permission === "granted") {
-        return new Notification(message);
-    }
-    else {
-        window.focus();
-        alert(message);
-        return null;
-    }
-}
-
 const ul = document.getElementById('sessions');
 const timer = document.getElementById('timer');
 const params = new URLSearchParams(location.search);
-let start = new Date();
 const DEFAULT = 25;
 let minutes = DEFAULT;
 try {
     const value = params.get('minutes') || DEFAULT;
     if(value === 'test') {
-        minutes = .1;
+        minutes = 1 / 6;
     }
     else {
         minutes = isNaN(value) || !(value >= 1) ? DEFAULT : parseInt(value);
@@ -40,47 +28,65 @@ let session = 1000 * 60 * minutes;
 const format = s => s.toString().padStart(2, '0');
 function updateTimeRemaining(time) {
     const ms = session - (time - start);
-    const raw = Math.trunc(ms / 1000);
-    const minutes = Math.trunc(raw / 60);
-    const seconds = raw % 60;
-    timer.textContent = `${format(minutes)}:${format(seconds)}`;
+    const seconds = Math.round(ms / 1000);
+    const minutes = Math.trunc(seconds / 60);
+    const remain = seconds % 60;
+    timer.textContent = `${format(minutes)}:${format(remain)}`;
 }
 
-updateTimeRemaining(start);
-startInterval();
+let start = null;
+function resetTimer() {
+    start = new Date();
+    updateTimeRemaining(start);
+}
+function updateTimer() {
+    const time = new Date();
+    updateTimeRemaining(time);
+    return time;
+}
 
-function startInterval() {
+startSession();
+
+function startSession() {
+    resetTimer();
     let interval = setInterval(() => {
-        const time = new Date();
-        updateTimeRemaining(time);
+        const time = updateTimer();
+
         if(time - start > session) {
             clearInterval(interval);
             ul.prepend(document.createElement('li'));
-            queueMicrotask(() => {
-                start = new Date();
-                updateTimeRemaining(start);
-
-                const next = () => {
-                    window.focus();
-                    start = new Date();
-                    updateTimeRemaining(start);
-                    startInterval();
-                    if(notification) {
-                        notification.close();
-                        notification.removeEventListener('click', next, { once: true });
-                        notification.removeEventListener('close', next, { once: true });
-                        document.removeEventListener('click', next, { once: true });
-                    }
-                }
-
-                const notification = notify('session complete');
-                if(!notification) next();
-                else {
-                    notification.addEventListener('click', next, { once: true });
-                    notification.addEventListener('close', next, { once: true });
-                    document.addEventListener('click', next, { once: true });
-                }
-            });
+            queueMicrotask(sessionComplete);
         }
     }, 1000);
+}
+
+function sessionComplete() {
+    resetTimer();
+    window.focus();
+    const notification = notify('session complete');
+    if(!notification) next();
+    else {
+        notification.addEventListener('click', next, { once: true });
+        notification.addEventListener('close', next, { once: true });
+        document.addEventListener('click', next, { once: true });
+    }
+
+    function next() {
+        window.focus();
+        startSession();
+        if(notification) {
+            notification.close();
+            notification.removeEventListener('click', next, { once: true });
+            notification.removeEventListener('close', next, { once: true });
+            document.removeEventListener('click', next, { once: true });
+        }
+    }
+}
+
+function notify(message) {
+    if(Notification.permission === "granted") {
+        return new Notification(message);
+    }
+    alert(message);
+    return null;
 }
