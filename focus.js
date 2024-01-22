7c3ae994364130b69696e1f3d5be32314ccc0627
Marty Nelson <marty@martypdx.com>
2024-01-22 09:14:37 -0800

    use enter key to lose focus on inputs

--- focus.js
+++ focus.js
@@ -4,7 +4,12 @@ document
     .forEach(node => {
         const key = `focus.${node.ariaLabel}`;
         node.innerHTML = localStorage.getItem(key) || '';
-        node.addEventListener('input', () => {
+        
+        node.addEventListener('input', (e) => { 
             localStorage.setItem(key, node.innerHTML);
+        });
+
+        node.addEventListener('keydown', ({ key }) => {
+            if(key === 'Enter' && document.hasFocus(node)) node.blur();
         })
     })
